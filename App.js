20e621ad702c1802552dabe3481eac1f2a7d7dd9
caste77an <hancastellan@naver.com>
2022-01-16 17:36:49 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>good</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
